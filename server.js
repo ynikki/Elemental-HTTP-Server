369f2aa62fb84ce799eb9b416b23c4bd533173ff
Nikki K <808nikz@gmail.com>
2016-07-08 19:02:10 -1000

    Something for Post

--- server.js
+++ server.js
@@ -4,15 +4,52 @@ var fs = require('fs');
 var http = require('http');
 var querystring = require('querystring');
 var url = require('url');
+//var htmlFileBody = ' ';
+var outputFile = ' ';
+var requestObj = "";
+var elements = "";
 
 var CONFIG = require('./config');
 
 
 var server = http.createServer( function (request, response) {
   // how you serve your client.
-  console.log('hello');
   response.writeHead(200, {'Content-Type' : 'text/html'});
   // handleRequest(request,response);
+  // function postHandler (request) {
+    // do POST stuff in here..
+    request.on('data', function (chunk) {
+      var requestFile = chunk.toString();
+      requestObj = querystring.parse(requestFile);
+      console.log(requestObj);
+    });
+
+    request.on('end', function () {
+      outputFile = "";
+      outputFile = requestObj.elementName + '.html';
+      var htmlFileBody = "";
+      htmlFileBody += '<!DOCTYPE html>\n';
+      htmlFileBody += '<html lang="en">\n';
+      htmlFileBody += '<head>\n';
+      htmlFileBody += '<meta charset="UTF-8">\n';
+      htmlFileBody += '<title>The Elements -' + ' ' + requestObj.elementName + '</title>\n';
+      htmlFileBody += '<link rel="stylesheet" href="/css/styles.css">\n';
+      htmlFileBody += '</head>\n';
+      htmlFileBody += '<body>\n';
+      htmlFileBody += '<h1>' + requestObj.elementName + '</h1>\n';
+      htmlFileBody += '<h2>' + requestObj.elementSymbol + '</h2>\n';
+      htmlFileBody += '<h3>' + requestObj.elementAtomicNumber + '</h3>\n';
+      htmlFileBody += '<p>' + requestObj.elementDescription + '</p>\n';
+      htmlFileBody += '<p><a href="/">back</a></p>\n';
+      htmlFileBody += '</body>\n';
+      htmlFileBody += '</html>\n';
+
+      fs.writeFile('./public/' + outputFile,htmlFileBody, 'utf8', function (err) {
+        console.log(outputFile);
+        console.log(htmlFileBody);
+       });
+    });
+  // }
 });
 
 server.on('request', handleRequest);
@@ -23,21 +60,14 @@ server.listen(CONFIG.PORT, function () {
 });
 
 function handleRequest (request, response) {
+  // console.log(request);
   if (request.method === 'GET') {
     getHandler(request, response);
     console.log('GET');
   } if (request.method === 'POST'){
     // do POST stuff in here..
-    postHandler(request);
+    // postHandler(request);
   }
-}
-
-function postHandler (request) {
-  // do POST stuff in here..
-  request.on('data', function (chunk) {
-    var requestFile = chunk.toString();
-    console.log(requestFile,"blah");
-  });
 }
 
 function getHandler (request, response) {
@@ -50,8 +80,9 @@ function getHandler (request, response) {
         response.write(errorFile);
       });
       return;
-    }
+    }  
     var fileContent = data.toString();
+    response.writeHead(200);
     response.write(fileContent);
     response.end();
   });
